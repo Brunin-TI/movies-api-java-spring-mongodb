cd874173557d0cdec619715b22a7541138f9461b
BruninTI <bruno.araujo@viannasempre.com.br>
2023-05-03 01:40:46 -0300

    implmented swagger

--- src/main/java/dev/torres/moviesapi/MoviesApiApplication.java
+++ src/main/java/dev/torres/moviesapi/MoviesApiApplication.java
@@ -1,9 +1,18 @@
 package dev.torres.moviesapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Movies API ", version = "3.0.1"),
+		servers = {
+				@Server(url = "http://localhost:8080")
+		}
+)
 public class MoviesApiApplication {
 
 	public static void main(String[] args) {
